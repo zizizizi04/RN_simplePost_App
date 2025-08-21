ab57bb15a196b4a9722f2c0f736246bc194935e9
zizizizi04 <joheuns@hanmail.net>
2025-08-21 14:28:45 +0900

    FlatList를 이용하여 게시글 화면 구성

--- app/(tabs)/posts.tsx
+++ app/(tabs)/posts.tsx
@@ -1,9 +1,59 @@
-import { Text, View } from "react-native";
+import { Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
 
 export default function posts() {
+  const posts: { id: number; title: string }[] = [
+    { id: 1, title: "게시글1" },
+    { id: 2, title: "게시글2" },
+    { id: 3, title: "게시글3" },
+    { id: 4, title: "게시글4" },
+    { id: 5, title: "게시글5" },
+    { id: 6, title: "게시글6" },
+    { id: 7, title: "게시글7" },
+    { id: 8, title: "게시글8" },
+    { id: 9, title: "게시글9" },
+    { id: 10, title: "게시글10" },
+  ];
+
   return (
-    <View>
-      <Text>posts</Text>
+    <View style={styles.postContainer}>
+      <FlatList
+        data={posts}
+        contentContainerStyle={styles.listWrap}
+        renderItem={({ item }) => (
+          <Text style={styles.postItem}>{item.title}</Text>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </View>
   );
 }
+
+const WIDTH = Dimensions.get("window").width;
+
+const styles = StyleSheet.create({
+  postContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  listWrap: {
+    width: WIDTH - 16,
+    paddingTop: 70,
+    paddingBottom: 16,
+    paddingHorizontal: 6,
+  },
+  postItem: {
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 10,
+    marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    height: 100,
+  },
+});
