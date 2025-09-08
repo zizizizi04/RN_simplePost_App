92e6a4ca9bbb1af2289f1347c55b965be1f47d18
zizizizi04 <joheuns@hanmail.net>
2025-09-08 11:35:57 +0900

    step2: PostWithContentDto 반영

--- app/(tabs)/posts/[id]/post.tsx
+++ app/(tabs)/posts/[id]/post.tsx
@@ -1,18 +1,31 @@
+import type { PostWithContentDto } from "@/types/post";
 import { useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
 
 export default function Post() {
   const { userId, id, title, body } = useLocalSearchParams();
+
+  const [post, setPost] = useState<PostWithContentDto | null>(null);
+
+  useEffect(() => {
+    setPost({
+      userId: Number(userId),
+      id: Number(id),
+      title: title as string, // title as string: 타입스크립트에서 타입을 강제로 변경
+      body: body as string,
+    });
+  }, []);
 
   return (
     <View style={styles.postContainer}>
       <View style={styles.postInner}>
         <View style={styles.postHeader}>
           <Text style={styles.postTitle}>제목</Text>
-          <Text style={styles.postTitleContetnt}>{title}</Text>
+          <Text style={styles.postTitleContetnt}>{post?.title}</Text>
         </View>
         <View style={styles.postBodyContainer}>
-          <Text style={styles.postBody}>{body}</Text>
+          <Text style={styles.postBody}>{post?.body}</Text>
         </View>
       </View>
     </View>
--- types/post.ts
+++ types/post.ts
@@ -4,3 +4,10 @@ export type PostDto = {
   title: string;
   body: string;
 };
+
+export type PostWithContentDto = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
