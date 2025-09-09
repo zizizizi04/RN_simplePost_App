2cf1886e00ff1535e245deb2f6f3be40186a669d
zizizizi04 <joheuns@hanmail.net>
2025-09-09 13:02:28 +0900

    글 상세보기 헤더 구현

--- app/(tabs)/posts/[id]/_layout.tsx
+++ app/(tabs)/posts/[id]/_layout.tsx
@@ -3,5 +3,5 @@ import { Stack, useLocalSearchParams } from "expo-router";
 export default function PostLayout() {
   const { postId } = useLocalSearchParams();
 
-  return <Stack screenOptions={{ title: `${postId}번 게시글` }} />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/(tabs)/posts/[id]/post.tsx
+++ app/(tabs)/posts/[id]/post.tsx
@@ -1,11 +1,14 @@
 import { db } from "@/firebase/config";
 import { PostWithContentDto } from "@/types/post";
-import { useLocalSearchParams } from "expo-router";
+import { Feather, MaterialCommunityIcons, Octicons } from "@expo/vector-icons";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import { Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Post() {
+  const router = useRouter();
+
   // useLocalSearchParams: 동적 라우팅을 위한 파라미터를 가져오는 함수
   // 파라미터값을 문자열로 취급
   const { postId } = useLocalSearchParams();
@@ -38,11 +41,35 @@ export default function Post() {
     <View style={styles.postContainer}>
       <View style={styles.postInner}>
         <View style={styles.postHeader}>
-          <Text style={styles.postTitle}>제목</Text>
-          <Text style={styles.postTitleContetnt}>{post?.title}</Text>
+          <View style={styles.postHeaderLeft}>
+            <Pressable onPress={() => router.back()}>
+              <Octicons name="chevron-left" size={24} color="black" />
+            </Pressable>
+          </View>
+          <View style={styles.postHeaderRight}>
+            <Pressable onPress={() => router.back()}>
+              <Octicons name="bell" size={24} color="black" />
+            </Pressable>
+            <Pressable onPress={() => router.back()}>
+              <Feather name="upload" size={24} color="black" />
+            </Pressable>
+            <Pressable onPress={() => router.back()}>
+              <MaterialCommunityIcons
+                name="dots-vertical"
+                size={24}
+                color="black"
+              />
+            </Pressable>
+          </View>
         </View>
-        <View style={styles.postBodyContainer}>
-          <Text style={styles.postBody}>{post?.content}</Text>
+        <View style={styles.postContentContainer}>
+          <View style={styles.contentHeader}></View>
+          <View style={styles.contentBody}>
+            <View style={styles.contentTitleWrap}></View>
+            <Text style={styles.postBody}>{post?.title}</Text>
+            <View style={styles.contentBodyWrap}></View>
+            <Text style={styles.postBody}>{post?.content}</Text>
+          </View>
         </View>
       </View>
     </View>
@@ -55,13 +82,11 @@ const styles = StyleSheet.create({
   postContainer: {
     flex: 1,
     alignItems: "center",
+    marginTop: 60,
   },
   postInner: {
-    width: WIDTH - 15,
-    padding: 16,
+    width: WIDTH,
     backgroundColor: "#fff",
-    borderRadius: 10,
-    marginBottom: 10,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -72,11 +97,19 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   postHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     borderBottomWidth: 1,
     borderBottomColor: "#ccc",
     paddingVertical: 10,
+    paddingHorizontal: 10,
   },
-
+  postHeaderLeft: {},
+  postHeaderRight: {
+    flexDirection: "row",
+    gap: 10,
+  },
   postTitle: {
     fontSize: 20,
     fontWeight: "bold",
@@ -85,11 +118,13 @@ const styles = StyleSheet.create({
     fontSize: 16,
     marginVertical: 5,
   },
-  postBodyContainer: {
+  postContentContainer: {
     margin: 5,
   },
   postBody: {
     fontSize: 16,
     marginTop: 5,
   },
+  contentHeader: {},
+  contentBody: {},
 });
